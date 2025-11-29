9ff52f6ec240b60ee89d16160a7a8abbd436731f
Your Name <you@example.com>
2025-11-29 16:02:10 +0530

    chore: prepare site for custom domain (root) and unregister old service workers

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Unregister any old service workers so clients don't keep stale cached index.html
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((regs) => {
+    regs.forEach((r) => r.unregister().catch(() => {}));
+  }).catch(() => {});
+}
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/rrnagar-frontend">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
