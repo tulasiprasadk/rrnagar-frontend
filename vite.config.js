129fce6cee4edced9265554852446e0997a4d19f
tulasiprasadk <tulasiprasadk@gmail.com>
2025-12-03 09:01:33 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-﻿import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const base = process.env.VITE_BASE_URL || '/';
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base,
   plugins: [react()],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+  base: '/rrnagar-frontend/',   // <-- add this line
+})
